f0e790d5805e815b86d159b2f7becee5b9cee983
Luis-Martinez <156262574+DMNT867@users.noreply.github.com>
2024-01-25 20:40:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 
+ document.addEventListener('DOMContentLoaded', function() {
+            
+            var emailRegex = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+            var passwordRegex = /^.{6,}$/;  // Al menos 6 caracteres
+
+            
+            function validarInicioSesion() {
+                
+                var correoElectronico = document.getElementById('correoElectronico').value;
+                var contrasena = document.getElementById('contrasena').value;
+
+                
+                if (!emailRegex.test(correoElectronico)) {
+                    alert('Email no válido');
+                } else if (!passwordRegex.test(contrasena)) {
+                    alert('Contraseña no válida (al menos 6 caracteres)');
+                } else {
+                    alert('Inicio de sesión exitoso');
+                    
+                }
+            }
+
+            
+            document.getElementById('btnIniciarSesion').addEventListener('click', validarInicioSesion);
+        });
