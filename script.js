7543ad6fb7f49b148730e38dc35fdd6e0bec2546
Luis-Martinez <156262574+DMNT867@users.noreply.github.com>
2024-01-25 21:42:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-    document.addEventListener('DOMContentLoaded', function() {
-            var emailRegex = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
-            var passwordRegex = /^.{6,}$/;
+function validarInicioSesion() {
+    console.log('Función validarInicioSesion llamada');  // Agrega esta línea
+    var correoElectronico = document.getElementById('correoElectronico').value;
+    var contrasena = document.getElementById('contrasena').value;
 
-            function validarInicioSesion() {
-                var correoElectronico = document.getElementById('correoElectronico').value;
-                var contrasena = document.getElementById('contrasena').value;
-
-                if (!emailRegex.test(correoElectronico)) {
-                    alert('Email no válido');
-                } else if (!passwordRegex.test(contrasena)) {
-                    alert('Contraseña no válida (al menos 6 caracteres)');
-                } else {
-                    alert('Inicio de sesión exitoso');
-                    // Puedes agregar aquí código adicional, como enviar el formulario.
-                }
-            }
-
-            document.getElementById('btnIniciarSesion').addEventListener('click', validarInicioSesion);
-        });
+    if (!emailRegex.test(correoElectronico)) {
+        alert('Email no válido');
+    } else if (!passwordRegex.test(contrasena)) {
+        alert('Contraseña no válida (al menos 6 caracteres)');
+    } else {
+        alert('Inicio de sesión exitoso');
+        // Puedes agregar aquí código adicional, como enviar el formulario.
+    }
+}
